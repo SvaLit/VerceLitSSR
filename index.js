f17de968bfa124192a83f85e09ad8ee222616549
PonomareVlad <Git@PonomareVlad.ru>
2021-05-07 01:53:24 +0500

    Not working pure version

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import {render} from "./@lit-labs/ssr/lib/render-with-global-dom-shim.js";
-import {html} from "./lit";
+import {render} from "@lit-labs/ssr/lib/render-with-global-dom-shim.js";
+import {html} from "lit";
 
 export default async (req, res) => {
     res.status(200);
--- lit/index.js
+++
@@ -1,2 +0,0 @@
-import"../lit-html";import"../@lit/reactive-element";export*from"../lit-element/lit-element.js";
-//# sourceMappingURL=index.js.map
