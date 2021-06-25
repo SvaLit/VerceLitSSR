4e0d6286102accb5c6979bb321019f17fed12687
PonomareVlad <Git@PonomareVlad.ru>
2021-06-26 04:18:38 +0500

    Refactor

--- loader/client.mjs
+++ loader/client.mjs
@@ -1,8 +1,5 @@
 import 'lit/experimental-hydrate-support.js';
-import {
-    hasNativeDeclarativeShadowRoots,
-    hydrateShadowRoots
-} from '@webcomponents/template-shadowroot/template-shadowroot.js';
+import {hydrateShadowRoots} from '@webcomponents/template-shadowroot/template-shadowroot.js';
 
 function supportsDeclarativeShadowDOM() {
     return HTMLTemplateElement.prototype.hasOwnProperty("shadowRoot");
