9d3446d170b6ddd269e22cc6ffb64dbc19f9e14e
PonomareVlad <Git@PonomareVlad.ru>
2021-07-04 05:42:58 +0500

    Fix

--- components/app.mjs
+++ components/app.mjs
@@ -2,3 +2,47 @@ import {LitElement, html} from "lit";
 import {until} from 'lit/directives/until.js';
 import {serverUntil} from "@lit-labs/ssr-client/directives/server-until.js";
 import 'isomorphic-fetch';
+
+export function isServer() {
+    return (typeof process !== 'undefined') && (process.release.name === 'node');
+}
+
+export async function fetchIP() {
+    return await fetch('https://api.ipify.org?format=json'/*, {mode: "no-cors"}*/).then(r => r.json()).then(r => r.ip);
+}
+
+export class AppDemo extends LitElement {
+    constructor() {
+        super();
+        this.url = '';
+        this.ip = isServer() ? fetchIP() : '';
+        this.hydrated = false;
+    }
+
+    static get properties() {
+        return {
+            url: {type: String},
+            hydrated: {type: Boolean}
+        }
+    }
+
+    async getIP() {
+        return await this.ip;
+    }
+
+    firstUpdated(_changedProperties) {
+        this.hydrated = true;
+        this.url = location.pathname;
+        this.ip = fetchIP();
+        super.firstUpdated(_changedProperties);
+    }
+
+    render() {
+        return html`<p>Path: ${this.url} ${this.hydrated ? '(Hydrated)' : ''}</p>
+        <p>Worker IP:
+            <span>${isServer() ? serverUntil(this.getIP(), 'Updating...') : until(this.getIP(), 'Updating...')}</span>
+        </p>`;
+    }
+}
+
+customElements.define('lit-app', AppDemo);
--- loader/client.mjs
+++ loader/client.mjs
@@ -1,12 +1,2 @@
 import 'lit/experimental-hydrate-support.js';
-import {hydrateShadowRoots} from '@webcomponents/template-shadowroot/template-shadowroot.js';
-
-function supportsDeclarativeShadowDOM() {
-    return HTMLTemplateElement.prototype.hasOwnProperty("shadowRoot");
-}
-
-if (!supportsDeclarativeShadowDOM()) {
-    hydrateShadowRoots(document.body);
-}
-
-import('/components/app.mjs');
+import '/components/app.mjs';
