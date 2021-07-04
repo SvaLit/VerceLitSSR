86b61ccf619d3ba36ccdc5c914edb7eb7852bc6a
PonomareVlad <Git@PonomareVlad.ru>
2021-07-04 22:58:50 +0500

    Added globalThis Polyfill

--- loader/server.mjs
+++ loader/server.mjs
@@ -14,7 +14,9 @@ export default async (req, res) => {
     return readableFrom(render(html`
         <!DOCTYPE html>
         <html lang="ru">${unsafeHTML(head.toString())}
-        <body><script src="https://unpkg.com/es-module-shims@0.12.1/dist/es-module-shims.min.js" async noshim></script>
+        <body>
+        <script src="https://polyfill.io/v3/polyfill.min.js?features=globalThis" async noshim></script>
+        <script src="https://unpkg.com/es-module-shims@0.12.1/dist/es-module-shims.min.js" async noshim></script>
         <lit-app url="${req.url}"><span style="color: coral">Not hydrated</span></lit-app>
         </body>
         </html>`), true).pipe(res);
