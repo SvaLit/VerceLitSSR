d3f9b75f48260e8bddcc7d38530c9e8a9d60103f
PonomareVlad <Git@PonomareVlad.ru>
2021-07-25 04:12:43 +0500

    Added isomorphic Realm test

--- components/app.mjs
+++ components/app.mjs
@@ -1,21 +1,29 @@
+import Realm from "realm";
 import {LitElement, html} from "lit";
 import {until} from '#isomorphic-until';
-import 'isomorphic-fetch';
 
 export function isServer() {
     return (typeof process !== 'undefined') && (process.release.name === 'node');
-}
-
-export async function fetchIP() {
-    return await fetch('https://api.ipify.org?format=json').then(r => r.json()).then(r => r.ip);
 }
 
 export class AppDemo extends LitElement {
     constructor() {
         super();
         this.url = '';
-        this.ip = isServer() ? fetchIP() : '';
         this.hydrated = false;
+        this.mongo = this.connectRealmMongoDB();
+        this.cache = null;
+    }
+
+    async connectRealmMongoDB() {
+        const app = new Realm.App({id: "test-jenyl"});
+        const credentials = Realm.Credentials.anonymous();
+        try {
+            const user = await app.logIn(credentials);
+            return app.currentUser.mongoClient("mongodb-atlas");
+        } catch (err) {
+            console.error("Failed to log in", err);
+        }
     }
 
     static get properties() {
@@ -25,20 +33,25 @@ export class AppDemo extends LitElement {
         }
     }
 
-    async getIP() {
-        return await this.ip;
+    async testRealm() {
+        if (this.cache) return this.cache;
+        const mongo = await this.mongo;
+        if (!mongo) return 'Error';
+        const test = mongo.db("test").collection("test");
+        return this.cache = (await test.findOne(isServer() ? {server: true} : {client: true})).content;
     }
 
     firstUpdated(_changedProperties) {
         this.hydrated = true;
         this.url = location.href.replace(location.origin, '');
-        this.ip = fetchIP();
         super.firstUpdated(_changedProperties);
     }
 
     render() {
         return html`<p>Path: ${this.url} (${this.hydrated ? html`<span style="color: greenyellow">Hydrated</span>` :
-                html`<slot></slot>`})</p><p><span style="color: gray">Worker IP:</span> ${until(this.getIP(), 'Updating...')}</p>`;
+                html`
+                    <slot></slot>`})</p><p><span style="color: gray">Content from DB:</span>
+            ${until(this.testRealm(), 'Loading...')}</p>`;
     }
 }
 
